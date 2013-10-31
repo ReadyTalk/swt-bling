a65c57646c19aae289849574d52f506a364a421f
Ben Limmer <ben@benlimmer.com>
2013-10-31 13:21:43 -0600

    This doesn't work quite right, but it's a start.

--- src/examples/java/com/readytalk/swt/widgets/notifications/BubbleExample.java
+++ src/examples/java/com/readytalk/swt/widgets/notifications/BubbleExample.java
@@ -1,5 +1,34 @@
 package com.readytalk.swt.widgets.notifications;
 
-public class BubbleExample {
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
+public class BubbleExample {
+  public static void main(String[] args) {
+    Display display = new Display();
+    Shell shell = new Shell(display);
+    shell.setLayout(new FillLayout());
+    Composite composite = new Composite(shell, SWT.NONE);
+    composite.setLayout(new GridLayout());
+
+    Button button = new Button(composite, SWT.PUSH);
+    button.setText("I Have a Bubble Tooltip");
+    button.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true));
+
+    Bubble bubble = new Bubble(button, "This text explains what's going to happen when you click the button.");
+
+    shell.setSize(200, 200);
+    shell.open();
+    while (!shell.isDisposed()) {
+      if (!display.readAndDispatch()) {
+        display.sleep();
+      }
+    }
+  }
 }
--- src/main/java/com/readytalk/swt/widgets/notifications/Bubble.java
+++ src/main/java/com/readytalk/swt/widgets/notifications/Bubble.java
@@ -1,8 +1,14 @@
 package com.readytalk.swt.widgets.notifications;
 
+import com.readytalk.swt.helpers.AncestryHelper;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.graphics.Region;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
@@ -10,16 +16,21 @@ import org.eclipse.swt.widgets.Widget;
 
 public class Bubble extends Widget {
   private static final RGB BACKGROUND_COLOR = new RGB(74, 74, 74);
+  private static final int TRIANGLE_WIDTH = 3; //pixels
+  private static final int TRIANGLE_HEIGHT = 3; //pixels
 
   private Listener listener;
   private String tooltipText;
-  private Shell tooltip;
+  private Control parent;
+  private Shell parentShell, tooltip;
 
-  public Bubble(Widget parent, String tooltipText, int style) {
-    super(parent, style);
+  public Bubble(Control parent, String tooltipText) {
+    super(parent, SWT.NONE);
 
+    this.parent = parent;
     this.tooltipText = tooltipText;
-    tooltip = new Shell((Shell) parent, SWT.ON_TOP | SWT.NO_TRIM);
+    parentShell = AncestryHelper.getShellFromControl(parent);
+    tooltip = new Shell(parentShell, SWT.ON_TOP | SWT.NO_TRIM);
     tooltip.setBackground(new Color(parent.getDisplay(), BACKGROUND_COLOR)); // TODO: we need to manage our colors onDispose
 
     listener = new Listener() {
@@ -40,6 +51,31 @@ public class Bubble extends Widget {
     addListener(SWT.Dispose, listener);
     tooltip.addListener(SWT.Paint, listener);
     tooltip.addListener(SWT.MouseDown, listener);
+
+
+    Region tooltipRegion = calculateTooltipRegion();
+    tooltip.open(); //TODO: this needs to be triggered somehow
+  }
+
+  private Region calculateTooltipRegion() {
+    // Find bottom centered point
+    // TODO: This won't always work, we might need to draw above the control sometimes
+    Rectangle controlBounds = parent.getBounds();
+    int centerPointX = (controlBounds.x + controlBounds.width) / 2;
+    int centerPointY = (controlBounds.y + controlBounds.height) / 2;
+
+    // Find the center point, then just create the polygon without Point objects for performance
+    int[] polygon = {
+      centerPointX, centerPointY,
+      centerPointX + TRIANGLE_WIDTH, centerPointY + TRIANGLE_HEIGHT,
+
+    };
+
+    return null;
+  }
+
+  public void checkSubclass() {
+    //no-op
   }
 
   private void onDispose(Event event) {
@@ -48,9 +84,20 @@ public class Bubble extends Widget {
 
   private void onPaint(Event event) {
     // TODO: paint things
+    GC gc = event.gc;
+
+    setShellSizeForText(gc, tooltipText);
+
+    gc.drawText(tooltipText, 0, 0);
   }
 
   private void onMouseDown(Event event) {
     // TODO: dismiss the tooltip if they click it
+  }
+
+  private void setShellSizeForText(GC gc, String text) {
+    Point textExtent = gc.textExtent(text);
+    parentShell.setSize(textExtent);
+    parentShell.layout();
   }
 }
