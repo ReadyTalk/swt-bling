ce9844fc5030fc744cfef5cb7538efe019af0789
Ben Limmer <ben@benlimmer.com>
2013-10-31 14:44:53 -0600

    Still a lot to be done, but looking better than before.

--- src/examples/java/com/readytalk/swt/widgets/notifications/BubbleExample.java
+++ src/examples/java/com/readytalk/swt/widgets/notifications/BubbleExample.java
@@ -1,6 +1,8 @@
 package com.readytalk.swt.widgets.notifications;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseTrackAdapter;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -11,17 +13,24 @@ import org.eclipse.swt.widgets.Shell;
 
 public class BubbleExample {
   public static void main(String[] args) {
-    Display display = new Display();
-    Shell shell = new Shell(display);
+    final Display display = new Display();
+    final Shell shell = new Shell(display);
     shell.setLayout(new FillLayout());
     Composite composite = new Composite(shell, SWT.NONE);
     composite.setLayout(new GridLayout());
 
-    Button button = new Button(composite, SWT.PUSH);
+    final Button button = new Button(composite, SWT.PUSH);
     button.setText("I Have a Bubble Tooltip");
     button.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true));
 
-    Bubble bubble = new Bubble(button, "This text explains what's going to happen when you click the button.");
+    final Bubble bubble = new Bubble(button, "This text explains what's going to happen when you click the button.");
+    // This will be eventually be handled by a controller of some sort
+    button.addMouseTrackListener(new MouseTrackAdapter() {
+      public void mouseHover(MouseEvent e) {
+        super.mouseHover(e);
+        bubble.show();
+      }
+    });
 
     shell.setSize(200, 200);
     shell.open();
--- src/main/java/com/readytalk/swt/widgets/notifications/Bubble.java
+++ src/main/java/com/readytalk/swt/widgets/notifications/Bubble.java
@@ -16,6 +16,9 @@ import org.eclipse.swt.widgets.Widget;
 
 public class Bubble extends Widget {
   private static final RGB BACKGROUND_COLOR = new RGB(74, 74, 74);
+  private static final RGB TEXT_COLOR = new RGB(204, 204, 204);
+  private static final int TEXT_HEIGHT_PADDING = 5; //pixels
+  private static final int TEXT_WIDTH_PADDING = 10; //pixels
   private static final int TRIANGLE_WIDTH = 3; //pixels
   private static final int TRIANGLE_HEIGHT = 3; //pixels
 
@@ -24,14 +27,20 @@ public class Bubble extends Widget {
   private Control parent;
   private Shell parentShell, tooltip;
 
+  private Rectangle rectangle;
+  private Color textColor;
+
   public Bubble(Control parent, String tooltipText) {
     super(parent, SWT.NONE);
 
     this.parent = parent;
     this.tooltipText = tooltipText;
     parentShell = AncestryHelper.getShellFromControl(parent);
+    parentShell.layout();
+
     tooltip = new Shell(parentShell, SWT.ON_TOP | SWT.NO_TRIM);
     tooltip.setBackground(new Color(parent.getDisplay(), BACKGROUND_COLOR)); // TODO: we need to manage our colors onDispose
+    textColor = new Color(parent.getDisplay(), TEXT_COLOR);
 
     listener = new Listener() {
       public void handleEvent(Event event) {
@@ -51,28 +60,51 @@ public class Bubble extends Widget {
     addListener(SWT.Dispose, listener);
     tooltip.addListener(SWT.Paint, listener);
     tooltip.addListener(SWT.MouseDown, listener);
-
-
-    Region tooltipRegion = calculateTooltipRegion();
-    tooltip.open(); //TODO: this needs to be triggered somehow
   }
 
-  private Region calculateTooltipRegion() {
-    // Find bottom centered point
-    // TODO: This won't always work, we might need to draw above the control sometimes
-    Rectangle controlBounds = parent.getBounds();
-    int centerPointX = (controlBounds.x + controlBounds.width) / 2;
-    int centerPointY = (controlBounds.y + controlBounds.height) / 2;
-
-    // Find the center point, then just create the polygon without Point objects for performance
-    int[] polygon = {
-      centerPointX, centerPointY,
-      centerPointX + TRIANGLE_WIDTH, centerPointY + TRIANGLE_HEIGHT,
-
-    };
-
-    return null;
+  public void show() {
+    Region toolTipRegion = new Region();
+    Point location = parent.getLocation();
+    Point textExtent = getTextSize(tooltipText);
+    rectangle = calculateRectangleRegion(location.x, location.y, textExtent);
+    toolTipRegion.add(rectangle);
+    tooltip.setRegion(toolTipRegion);
+    Rectangle regionBounds = toolTipRegion.getBounds();
+//    tooltip.setSize(regionBounds.width, regionBounds.height);
+    tooltip.setLocation(location);
+    tooltip.open();
   }
+
+  private Rectangle calculateRectangleRegion(int parentLocationX, int parentLocationY, Point textExtent) {
+    return new Rectangle(parentLocationX,
+            parentLocationY,
+            textExtent.x + TEXT_WIDTH_PADDING,
+            textExtent.y + TEXT_HEIGHT_PADDING);
+  }
+
+//  private int[] calculateTooltipRegion() {
+//    // Find bottom centered point
+//    // TODO: This won't always work, we might need to draw above the control sometimes
+//    Rectangle controlBounds = parent.getBounds();
+//    int centerPointX = (controlBounds.x + controlBounds.width) / 2;
+//    int centerPointY = (controlBounds.y + controlBounds.height) / 2;
+//
+//    Point textExtent = getTextSize(tooltipText);
+//
+//    // Find the center point, then just create the polygon without Point objects for performance
+//    int[] polygon = {
+//      centerPointX, centerPointY,
+//      centerPointX + TRIANGLE_WIDTH, centerPointY + TRIANGLE_HEIGHT,
+//      centerPointX + (textExtent.x / 2), centerPointY + TRIANGLE_HEIGHT,
+//      centerPointX + (textExtent.x / 2), centerPointY + TRIANGLE_HEIGHT + textExtent.y,
+//      centerPointX - (textExtent.x / 2), centerPointY + TRIANGLE_HEIGHT + textExtent.y,
+//      centerPointX - (textExtent.x / 2), centerPointY + TRIANGLE_HEIGHT,
+//      centerPointX - TRIANGLE_WIDTH, centerPointY - TRIANGLE_HEIGHT,
+//      centerPointX, centerPointY
+//    };
+//
+//    return polygon;
+//  }
 
   public void checkSubclass() {
     //no-op
@@ -83,21 +115,20 @@ public class Bubble extends Widget {
   }
 
   private void onPaint(Event event) {
-    // TODO: paint things
     GC gc = event.gc;
 
-    setShellSizeForText(gc, tooltipText);
-
-    gc.drawText(tooltipText, 0, 0);
+    gc.setForeground(textColor);
+    gc.drawText(tooltipText, rectangle.x + (TEXT_WIDTH_PADDING / 2), rectangle.y + (TEXT_HEIGHT_PADDING / 2));
   }
 
   private void onMouseDown(Event event) {
     // TODO: dismiss the tooltip if they click it
   }
 
-  private void setShellSizeForText(GC gc, String text) {
+  private Point getTextSize(String text) {
+    GC gc = new GC(parent.getDisplay());
     Point textExtent = gc.textExtent(text);
-    parentShell.setSize(textExtent);
-    parentShell.layout();
+    gc.dispose();
+    return textExtent;
   }
 }
