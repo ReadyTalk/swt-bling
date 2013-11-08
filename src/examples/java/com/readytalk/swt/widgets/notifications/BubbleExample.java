2843a1b0b9aef11b164d6350ecd9fd70fb08727b
Ben Limmer <ben@benlimmer.com>
2013-11-07 19:15:19 -0700

    Updated the Bubble Example to show off tags. This commit closes #12. This does, however, bring up the point that Bubbles can overlap one another. There's also not an obvious time to dismiss all Bubbles by tag. I'd like to chat with the team about how to handle this.

--- src/examples/java/com/readytalk/swt/widgets/notifications/BubbleExample.java
+++ src/examples/java/com/readytalk/swt/widgets/notifications/BubbleExample.java
@@ -3,6 +3,8 @@ package com.readytalk.swt.widgets.notifications;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseTrackAdapter;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -17,16 +19,48 @@ public class BubbleExample {
     final Shell shell = new Shell(display);
     shell.setLayout(new FillLayout());
     Composite composite = new Composite(shell, SWT.NONE);
-    composite.setLayout(new GridLayout());
+    composite.setLayout(new GridLayout(3, false));
 
-    final Button button = new Button(composite, SWT.PUSH);
+    final BubbleRegistry bubbleRegistry = BubbleRegistry.getInstance();
+
+    Button displayNewBubbles = new Button(composite, SWT.PUSH);
+    displayNewBubbles.setText("Display Bubbles Tag NEW");
+    Bubble displayNewBubblesBubble = new Bubble(displayNewBubbles, "Click here to display all Bubbles of type \"NEW\"");
+    bubbleRegistry.register(displayNewBubbles, displayNewBubblesBubble);
+    displayNewBubbles.addSelectionListener(new SelectionAdapter() {
+      public void widgetSelected(SelectionEvent e) {
+        bubbleRegistry.showBubblesByTags(BubbleTag.NEW);
+      }
+    });
+
+    Button displayAllBubbles = new Button(composite, SWT.PUSH);
+    displayAllBubbles.setText("Display All Bubbles");
+    Bubble displayAllBubblesBubble = new Bubble(displayAllBubbles, "Click here to display all Bubbles");
+    bubbleRegistry.register(displayAllBubbles, displayAllBubblesBubble);
+    displayAllBubbles.addSelectionListener(new SelectionAdapter() {
+      public void widgetSelected(SelectionEvent e) {
+        bubbleRegistry.showAllBubbles();
+      }
+    });
+
+    Button dismissAllBubbles = new Button(composite, SWT.PUSH);
+    dismissAllBubbles.setText("Dismiss All Bubbles");
+    Bubble hideAllBubblesBubble = new Bubble(dismissAllBubbles, "Click here to dismiss all Bubbles");
+    bubbleRegistry.register(dismissAllBubbles, hideAllBubblesBubble);
+    dismissAllBubbles.addSelectionListener(new SelectionAdapter() {
+      public void widgetSelected(SelectionEvent e) {
+        bubbleRegistry.dismissAllBubbles();
+      }
+    });
+
+    Button button = new Button(composite, SWT.PUSH);
     button.setText("I Have a Bubble Tooltip");
     button.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true));
 
-    final Bubble bubble = new Bubble(button, "This text explains what's going to happen when you click the button.");
-    BubbleRegistry.getInstance().register(button, bubble);
+    Bubble bubble = new Bubble(button, "This text explains a new, exciting feature!");
+    BubbleRegistry.getInstance().register(button, bubble, BubbleTag.NEW);
 
-    shell.setSize(200, 200);
+    shell.pack();
     shell.open();
     while (!shell.isDisposed()) {
       if (!display.readAndDispatch()) {
