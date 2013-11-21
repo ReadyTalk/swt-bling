5fead5ddcf63ad648c54443d46778eff915f7c9c
Ben Limmer <ben@benlimmer.com>
2013-11-21 11:48:42 -0700

    Updated Javadoc for AncestryHelper. Related to #25.

--- src/main/java/com/readytalk/swt/helpers/AncestryHelper.java
+++ src/main/java/com/readytalk/swt/helpers/AncestryHelper.java
@@ -3,7 +3,17 @@ package com.readytalk.swt.helpers;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 
+/**
+ * Helper methods to answer common ancestry questions related to SWT components.<br/>
+ * <br/>
+ * Methods in this class should be <code>static</code>.
+ */
 public class AncestryHelper {
+  /**
+   * Helper method to determine the shell an SWT <code>Control</code> element belongs to.
+   * @param control The <code>Control</code> element you want to determine what Shell it belongs to.
+   * @return The <code>Shell</code> that the <code>Control</code> belongs to.
+   */
   public static Shell getShellFromControl(Control control) {
     if (control == null) {
       return null;
