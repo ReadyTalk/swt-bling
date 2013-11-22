7cdf88037f6608fd1c2ca1dcc35c534a048cced0
Ben Limmer <ben@benlimmer.com>
2013-11-22 15:37:42 -0700

    More Javadoc for components. Related to #25.

--- src/main/java/com/readytalk/swt/helpers/DebugHelper.java
+++ src/main/java/com/readytalk/swt/helpers/DebugHelper.java
@@ -1,8 +1,16 @@
 package com.readytalk.swt.helpers;
 
+/**
+ * Helpers useful in various debugging scenarios.
+ */
 public class DebugHelper {
   private static final String NEWLINE = System.getProperty("line.separator");
 
+  /**
+   * Get the stack trace from a Throwable exception as a String. Useful in printing log statements.
+   * @param t The <code>Throwable</code> that you want to extract the stacktrace from
+   * @return The extracted stacktrace
+   */
   public static String getStackTraceAsString(Throwable t) {
     StringBuilder buffer = new StringBuilder();
 
