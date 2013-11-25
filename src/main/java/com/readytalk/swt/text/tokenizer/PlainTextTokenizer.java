8a30a12bcf4a469884cd5bd92296a18c025984d6
steel <steel@kryas.com>
2013-11-25 01:31:18 -0700

    clean up

--- src/main/java/com/readytalk/swt/text/tokenizer/PlainTextTokenizer.java
+++ src/main/java/com/readytalk/swt/text/tokenizer/PlainTextTokenizer.java
@@ -7,33 +7,33 @@ import java.util.List;
 import com.readytalk.swt.text.painter.TextType;
 
 class PlainTextTokenizer implements TextTokenizer {
-  
+
   private List<TextToken> tokens = new ArrayList<TextToken>();
-  private Charset encoding = Charset.defaultCharset(); 
-      
+  private Charset encoding = Charset.defaultCharset();
+
   @Override
   public TextTokenizer setEncoding(Charset encoding) {
     this.encoding = encoding;
     return this;
   }
-  
+
   @Override
   public Charset getEncoding() {
     return encoding;
   }
-  
+
   @Override
   public TextTokenizer reset() {
     tokens.clear();
     return this;
   }
-  
+
   public List<TextToken> tokenize(String text) {
-	
-	if(text == null || "".equals(text)) {
-		return tokens;
-	}
-	
+
+    if (text == null || "".equals(text)) {
+      return tokens;
+    }
+
     TextToken textToken = new TextToken(TextType.TEXT, text);
     tokens.add(textToken);
     return tokens;
