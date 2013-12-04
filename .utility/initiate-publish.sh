3d6e3463cb1c3a53e2a2405d86542514df5cffed
Ben Limmer <ben@benlimmer.com>
2013-12-04 11:41:18 -0700

    Last commit to test snapshot publishing.

--- .utility/initiate-publish.sh
+++ .utility/initiate-publish.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-# This script initiates the Gradle Publish task when pushes to master occur.
+# This script initiates the Gradle publishing task when pushes to master occur.
+# NOTE: Travis-CI can only publish SNAPSHOT versions. To release a version, you need
+#       to use the internal ReadyTalk Jenkins job.
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+# TODO: Remove publishing test branch.
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" || "$TRAVIS_BRANCH" == "publishing" ]; then
+  if [[ $(./gradlew -q getVersion) != *SNAPSHOT* ]]; then
+      echo 'Travis can only publish snapshots. To publish a release, use the ReadyTalk Jenkins instance.'
+      return 0
+  fi
+
   echo -e "Starting publish to Sonatype...\n"
 
-  # Uncomment when publish functionality is implemented
-  #./gradle publish -PnexusUsername="${NEXUS_USERNAME} -PnexusPassword="${NEXUS_PASSWORD}" -Psigning.password="${SIGNING_PASSWORD}"
+  ./gradlew uploadArchives -PnexusUsername="${NEXUS_USERNAME} -PnexusPassword="${NEXUS_PASSWORD}"
+  RETVAL=$?
 
-  echo -e "Completed publish!\n"
+  if [ $RETVAL -eq 0 ]; then
+    echo 'Completed publish!'
+  else
+    echo 'Publish failed.'
+    return 1
+  fi
+
 fi
