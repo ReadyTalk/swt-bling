3b9e9e0be1cbcbb0ffc13cd48914e68a6431d38e
Matt Weaver <ezweave@gmail.com>
2014-01-10 10:41:58 -0700

    fixed toggle, added example of radio like buttons, and a little bit of integ/unit testing

--- src/integTest/java/com/readytalk/swt/widgets/buttons/SquareButtonIntegTest.java
+++ src/integTest/java/com/readytalk/swt/widgets/buttons/SquareButtonIntegTest.java
@@ -25,8 +25,39 @@ public class SquareButtonIntegTest {
 
     shell.close();
     display.dispose();
+  }
 
+  @Test
+  public void testSizing() {
 
   }
 
+  @Test
+  public void testSizeHint() {
+
+  }
+
+  @Test
+  public void testToggleable() {
+    Display display = Display.getDefault();
+    Shell shell = new Shell(display);
+
+    shell.open();
+
+    SquareButton button = new SquareButton(shell, SWT.CENTER);
+    button.setText(buttonText);
+    button.setToggleable(true);
+
+    Assert.assertTrue(button.isToggleable());
+
+    button.setToggled(true);
+    Assert.assertTrue(button.isToggled());
+
+
+    button.setToggled(false);
+    Assert.assertFalse(button.isToggled());
+
+    shell.close();
+    display.dispose();
+  }
 }
