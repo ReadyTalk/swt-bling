a580f60756b5140e483adfa38bdfad1bf26ce4ed
stan.steel <stan.steel@readytalk.com>
2014-01-21 15:03:16 -0700

    added comment regarding util method

--- src/main/java/com/readytalk/swt/util/ResourceQuery.java
+++ src/main/java/com/readytalk/swt/util/ResourceQuery.java
@@ -2,13 +2,21 @@ package com.readytalk.swt.util;
 
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.swt.graphics.DeviceData;
 import org.eclipse.swt.widgets.Display;
 
+
+/**
+ * ResourceQuery returns mmp of key value pairs of the count results
+ * contained in the DeviceData array passed into display.  DeviceData
+ * will hold references to resources; so this should probably not be used
+ * in production.
+ */
 public class ResourceQuery {
 
-  public static HashMap<String, Integer> getAllocatedResourceCounts(final Display display) {
+  public static Map<String, Integer> getAllocatedResourceCounts(final Display display) {
     HashMap<String, Integer> results = new HashMap<String, Integer>();
     DeviceData data = display.getDeviceData();
     for (Object object : data.objects) {
