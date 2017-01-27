4f8f784f78933d797da3b461921896380ee8da60
Dain <dain.darnell@readytalk.com>
2017-01-27 12:37:10 -0700

    Simplify font loading
    Rather than first extracting the font resource to a file prior to
    loading, a font resource will now be loaded directly.

--- src/main/java/com/readytalk/swt/font/FontLoader.java
+++ src/main/java/com/readytalk/swt/font/FontLoader.java
@@ -1,159 +1,39 @@
 package com.readytalk.swt.font;
 
-import com.google.common.annotations.VisibleForTesting;
-import lombok.extern.java.Log;
 import org.eclipse.swt.widgets.Display;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.logging.Level;
-
 /**
- * A utility class for loading custom font files from a class resource into SWT
+ * A utility class for loading custom fonts into SWT
  */
-@Log
 public class FontLoader {
-  private static final int BUFFER_SIZE = 1024 * 4;
-
-  @VisibleForTesting
-  final Set<File> extractedFiles = new HashSet<File>();
-
   private final Display display;
-  private final String extractionPath;
 
   /**
-   * TODO
+   * Creates an instance of FontLoader with the specified Display
    *
    * @param display
-   * @param extractionPath
    */
-  public FontLoader(Display display, String extractionPath) {
+  public FontLoader(Display display) {
     this.display = display;
-    this.extractionPath = extractionPath;
-
-    // Dispose all cached font resources on exit and clean up extracted files if possible
-    if (display != null && !display.isDisposed()) {
-      display.disposeExec(new Runnable() {
-        @Override
-        public void run() {
-          deleteExtractedFiles();
-        }
-      });
-    }
   }
 
   /**
-   * Extract a font from a resource fontFileResourcePath to a temporary file and load it into SWT
+   * Load a font from a resource located at fontFileResourcePath into SWT
    *
    * @param fontFileResourcePath
-   * @return
+   * @return true if the font was successfully loaded, otherwise false
    */
-  public boolean loadFont(String fontFileResourcePath, String fontFileName) {
-    final InputStream fontFileResourceInputStream = FontLoader.class.getClassLoader()
-        .getResourceAsStream(fontFileResourcePath + fontFileName);
-    return loadFont(fontFileName, fontFileResourceInputStream);
+  public boolean loadFontResource(String fontFileResourcePath) {
+    return display.loadFont(FontLoader.class.getClassLoader().getResource(fontFileResourcePath).getPath());
   }
 
   /**
-   * Extract a font from a resource named fontFileName to a temporary file in the directory at extractionPath and
-   * load it into SWT
-   * <p>
-   * Used for testing.
+   * Load a font from a file located at fontFilePath into SWT
    *
-   * @param fontFileName
-   * @param fontInputStream
-   * @return
+   * @param fontFilePath
+   * @return true if the font was successfully loaded, otherwise false
    */
-  @VisibleForTesting
-  boolean loadFont(String fontFileName, InputStream fontInputStream) {
-    // SWT requires the font be loaded from a file on disk so we extract the font resource to a temp file
-    final File fontFile = new File(extractionPath, fontFileName);
-    final boolean extractSuccess = extractFontFile(fontFile, fontInputStream);
-
-    return extractSuccess && display.loadFont(fontFile.getAbsolutePath());
-  }
-
-  /**
-   * Extracts a font file resource from the classpath to fontFile, creating any directories if they don't exist,
-   * and then copies the contents of the resource to the file
-   *
-   * @param fontFile
-   * @param in
-   * @return
-   */
-  @VisibleForTesting
-  boolean extractFontFile(File fontFile, InputStream in) {
-    boolean success = false;
-
-    if (in != null) {
-      try {
-        if (fontFile.exists()) {
-          log.info("Deleting existing font file " + fontFile.getAbsolutePath());
-          fontFile.delete();
-        }
-
-        log.info("Extracting font file to: " + fontFile.getAbsolutePath());
-
-        fontFile.getParentFile().mkdirs();
-        fontFile.createNewFile();
-
-        final OutputStream out = new FileOutputStream(fontFile);
-
-        try {
-          copy(in, out);
-          success = true;
-          extractedFiles.add(fontFile);
-        } finally {
-          out.close();
-        }
-      } catch (IOException e) {
-        log.log(Level.WARNING, "Exception loading font from file: " + fontFile.getAbsolutePath(), e);
-      }
-    } else {
-      log.warning("Unable to get font file resource as stream: " + fontFile.getAbsolutePath());
-    }
-
-    return success;
-  }
-
-  /**
-   * Remove any font files extracted to the file system
-   */
-  public void deleteExtractedFiles() {
-    for (File file : extractedFiles) {
-      if (file != null && file.exists()) {
-        log.info("Deleting extracted font file: " + file);
-        file.delete();
-      }
-    }
-
-    extractedFiles.clear();
-  }
-
-  /**
-   * Copy bytes from an InputStream to an OutputStream
-   *
-   * @param input  the InputStream to read from
-   * @param output the OutputStream to write to
-   * @return the number of bytes copied
-   */
-  @VisibleForTesting
-  static long copy(InputStream input, OutputStream output) throws IOException {
-    final byte[] buffer = new byte[BUFFER_SIZE];
-    long count = 0;
-    int n;
-
-    while (-1 != (n = input.read(buffer))) {
-      output.write(buffer, 0, n);
-      count += n;
-    }
-
-    return count;
+  public boolean loadFontFile(String fontFilePath) {
+    return display.loadFont(fontFilePath);
   }
 }
--- src/test/java/com/readytalk/swt/font/FontLoaderTest.java
+++ src/test/java/com/readytalk/swt/font/FontLoaderTest.java
@@ -1,115 +1,50 @@
 package com.readytalk.swt.font;
 
-import com.google.common.io.Files;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Font;
-import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.widgets.Display;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.UUID;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class FontLoaderTest {
-  private static final String TEST_STRING = "It was the best of times, it was the blurst of times";
-  private static final String FONT_NAME = "Super Ultra Comic Sans Plus Plus Monospace";
-  private static final int STYLE = SWT.BOLD;
-
-  private final UUID TEST_ID = UUID.randomUUID();
-  private final String TEST_FONT_FILE_PATH = Files.createTempDir().getAbsolutePath();
+  private static final String TEST_FONT_RESOURCE_PATH = "fonts" + File.separator + "FontAwesome.otf";
+  private static final String TEST_FONT_FILE_PATH = "src" + File.separator + "test" + File.separator + "resources" +
+      File.separator + TEST_FONT_RESOURCE_PATH;
 
   private FontLoader fontLoader;
 
   @Before
   public void setup() {
-    fontLoader = new FontLoader(Display.getDefault(), TEST_FONT_FILE_PATH);
+    fontLoader = new FontLoader(Display.getDefault());
   }
 
   @After
   public void tearDown() {
-    fontLoader.deleteExtractedFiles();
-
-    // Clean up extracted font files
-    File fontFileDir = new File(TEST_FONT_FILE_PATH);
-    fontFileDir.delete();
-
     // Ensure a fresh Display for each test
     Display.getDefault().dispose();
   }
 
-  @Test
-  public void extractFontFile_validFontName_fontFileExtracted() {
-    final File fontFile = new File(TEST_FONT_FILE_PATH + File.separator + "TestFontFile_" + TEST_ID);
-    final InputStream input = new ByteArrayInputStream(TEST_STRING.getBytes());
-
-    removeFile(fontFile);
-
-    assertTrue(fontLoader.extractFontFile(fontFile, input));
-    assertEquals(TEST_STRING.getBytes().length, fontFile.length());
-
-    removeFile(fontFile);
+  @Test(expected = NullPointerException.class)
+  public void loadFontResource_invalidResource_throwException() {
+    fontLoader.loadFontResource("bogus/resource/path");
   }
 
   @Test
-  public void copy_validInput_validOutput() throws Exception {
-    final InputStream input = new ByteArrayInputStream(TEST_STRING.getBytes());
-    final OutputStream output = new ByteArrayOutputStream();
-
-    FontLoader.copy(input, output);
-
-    assertEquals(TEST_STRING, output.toString());
+  public void loadFontFile_invalidFile_returnFalse() {
+    assertFalse(fontLoader.loadFontFile("bogus/file/path"));
   }
-
-
 
   @Test
-  public void deleteExtractedFiles_fontFilesExtracted_extractedFilesListEmpty() {
-    fontLoader.loadFont(TEST_FONT_FILE_PATH, new ByteArrayInputStream(TEST_STRING.getBytes()));
-
-    assertFalse(fontLoader.extractedFiles.isEmpty());
-    assertEquals(1, new File(TEST_FONT_FILE_PATH).list().length);
-
-    fontLoader.deleteExtractedFiles();
-
-    assertTrue(fontLoader.extractedFiles.isEmpty());
-    assertEquals(0, new File(TEST_FONT_FILE_PATH).list().length);
+  public void loadFontResource_validResource_loadFontReturnTrue() throws Exception {
+    assertTrue(fontLoader.loadFontResource(TEST_FONT_RESOURCE_PATH));
   }
 
-  private void removeFile(File file) {
-    if (file.exists()) {
-      file.delete();
-    }
+  @Test
+  public void loadFontFile_validFile_loadFontReturnTrue() throws Exception {
+    assertTrue(fontLoader.loadFontFile(TEST_FONT_FILE_PATH));
   }
-
-  private boolean isFontDataMatch(Font font, String name, int size, int style) {
-    final FontData fontData = font.getFontData()[0];
-    return name.equals(fontData.getName()) && size == fontData.getHeight() && style == fontData.getStyle();
-  }
-
-  /*private void verifyFontFileDirectory() {
-    final File fontDir = new File(TEST_FONT_FILE_PATH);
-    final ImmutableList<String> fontFilesInDirectory = ImmutableList.copyOf(fontDir.list());
-
-    assertEquals(FontLoader.DEFAULT_FONT_FILE_NAMES.length, fontFilesInDirectory.size());
-
-    for (String fontName : FontLoader.DEFAULT_FONT_FILE_NAMES) {
-      assertTrue(fontFilesInDirectory.contains(fontName));
-    }
-  }
-
-  private void verifyDefaultFontsLoaded() {
-    for (String fontName : FontLoader.DEFAULT_FONT_NAMES) {
-      assertTrue(Display.getCurrent().getFontList(fontName, true).length > 0);
-    }
-  }*/
 }
