8a0542c5b1eddc93525ad58640856ab4b8430349
Dain <dain.darnell@readytalk.com>
2017-02-03 12:54:29 -0700

    Restore font reource extracting and loading
    It is actually necessary to have a file on disk in order to get SWT to
    load a font file path.

--- src/integTest/java/com/readytalk/swt/font/FontLoaderTest.java
+++ src/integTest/java/com/readytalk/swt/font/FontLoaderTest.java
@@ -1,29 +1,50 @@
 package com.readytalk.swt.font;
 
+import com.google.common.io.Files;
 import org.eclipse.swt.widgets.Display;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class FontLoaderTest {
-  private static final String TEST_FONT_RESOURCE_PATH = "fonts" + File.separator + "FontAwesome.otf";
-  private static final String TEST_FONT_FILE_PATH = "src" + File.separator + "test" + File.separator + "resources" +
+  private static final String TEST_STRING = "It was the best of times, it was the blurst of times";
+  private static final String TEST_FONT_FILE_NAME = "FontAwesome.otf";
+  private static final String TEST_FONT_RESOURCE_DIR = "fonts";
+  private static final String TEST_FONT_RESOURCE_PATH = TEST_FONT_RESOURCE_DIR + File.separator + TEST_FONT_FILE_NAME;
+  private static final String TEST_FONT_FILE_PATH = "src" + File.separator + "integTest" + File.separator + "resources" +
       File.separator + TEST_FONT_RESOURCE_PATH;
+
+  private String fontFileExtractPath;
+  private UUID testId;
 
   private FontLoader fontLoader;
 
   @Before
   public void setup() {
+    fontFileExtractPath = Files.createTempDir().getAbsolutePath();
+    testId = UUID.randomUUID();
     fontLoader = new FontLoader(Display.getDefault());
   }
 
   @After
   public void tearDown() {
+    fontLoader.deleteExtractedFiles();
+
+    // Clean up extracted font file directory
+    File fontFileDir = new File(fontFileExtractPath);
+    removeFile(fontFileDir);
+
     // Ensure a fresh Display for each test
     Display.getDefault().dispose();
   }
@@ -46,5 +67,52 @@ public class FontLoaderTest {
   @Test
   public void loadFontFile_validFile_loadFontReturnTrue() throws Exception {
     assertTrue(fontLoader.loadFontFile(TEST_FONT_FILE_PATH));
+  }
+
+  @Test
+  public void extractFontFile_validFontName_fontFileExtracted() {
+    final File fontFile = new File(fontFileExtractPath + File.separator + "TestFontFile_" + testId);
+    final InputStream input = new ByteArrayInputStream(TEST_STRING.getBytes());
+
+    removeFile(fontFile);
+
+    assertTrue(fontLoader.extractFontFile(fontFile, input));
+    assertEquals(TEST_STRING.getBytes().length, fontFile.length());
+
+    removeFile(fontFile);
+  }
+
+  @Test
+  public void extractAndLoadFontResource_validFontResource_fontExtractedAndLoaded() {
+    assertTrue(fontLoader.extractAndLoadFontResource(TEST_FONT_RESOURCE_PATH, TEST_FONT_FILE_NAME, fontFileExtractPath));
+  }
+
+  @Test
+  public void deleteExtractedFiles_fontFilesExtracted_extractedFilesListEmpty() {
+    fontLoader.extractAndLoadFontResource(TEST_FONT_RESOURCE_PATH, TEST_FONT_FILE_NAME, fontFileExtractPath);
+
+    assertFalse(fontLoader.extractedFiles.isEmpty());
+    assertEquals(1, new File(fontFileExtractPath).list().length);
+
+    fontLoader.deleteExtractedFiles();
+
+    assertTrue(fontLoader.extractedFiles.isEmpty());
+    assertEquals(0, new File(fontFileExtractPath).list().length);
+  }
+
+  @Test
+  public void copy_validInput_validOutput() throws Exception {
+    final InputStream input = new ByteArrayInputStream(TEST_STRING.getBytes());
+    final OutputStream output = new ByteArrayOutputStream();
+
+    fontLoader.copy(input, output);
+
+    assertEquals(TEST_STRING, output.toString());
+  }
+
+  private void removeFile(File file) {
+    if (file.exists()) {
+      file.delete();
+    }
   }
 }
